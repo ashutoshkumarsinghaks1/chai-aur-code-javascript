9b269528c4b9d6b34b9daf873f1be61942e9dbdf
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-10-22 12:58:04 +0530

    Add files via upload

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,69 @@
+/*
+*NOTE: javascript is a dynamic type language .
+it is because => You can create new variables at runtime, and the type of variables is determined at runtime.
+*/
+
+// Primitive
+// 7 types: String, Number , Boolean , null , undefined , Symbol , BigInt
+
+const score = 100; //number
+const scoreValue = 100.3;
+const loggedIn = false; //boolean
+const outsideTemp = null; // typeof => object
+let userEmail; //undefined
+let userEmail1 = undefined; //undefined
+
+const id = Symbol("123");
+const anotherid = Symbol("123");
+console.log(id === anotherid); // false   => these are not equal  //symbol
+const BigNumber = 33333333333336677788n; //BigInt
+//Rerence(Non Primitive)
+// Objects , Array , Functions
+const heros = ["Shaktiman", "naagraj", "doga"]; //Array
+
+let myObj = {
+  name: "Ashutosh", //object
+  age: 22,
+};
+
+//function definition: function(){}
+const myFunction = function () {
+  console.log("Hello World");
+};
+
+// we can check the datatype by using => typeof
+// now will check data type of each of above variable declared.
+console.log(typeof score); //number
+console.log(typeof scoreValue); //number
+console.log(typeof loggedIn); //boolean
+console.log(typeof outsideTemp); //obect  =>null
+console.log(typeof userEmail); //udefined
+console.log(typeof userEmail1); //undefined
+console.log(typeof id); //symbol
+console.log(typeof anotherid); //symbol
+console.log(typeof BigNumber); //bigint
+console.log(typeof heros); //object => Array
+console.log(typeof myObj); //boject =>object
+console.log(typeof myFunction); //function =>function
+
+//+++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap(Non-Primitive)
+let myYoutubename = "hiteshchoudrydotcom";
+
+let anothername = myYoutubename;
+anothername = "chaiaurcode";
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+  email: "user@google.com",
+  upi: "user@ybl",
+};
+
+let userTwo = userOne;
+
+userTwo.email = "hitesh@google.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
