3dcd15df9d77c6e523e40473bf689b19502610ab
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-11-10 09:28:11 +0530

    Update 07_dates_in_js.js

--- 01_basics/07_dates_in_js.js
+++ 01_basics/07_dates_in_js.js
@@ -1,29 +1,42 @@
 // Dates
 
-let myDate = new Date();
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
 
-// let myCreatedDate = new Date(2023, 0, 23)
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023");
+//let myDate = new Date();
+//console.log(myDate.toString());
+// console.log(typeof myDate);  //output :object
+// console.log(myDate.toISOString());
+// console.log(myDate.getFullYear());
+// console.log(myDate.getMonth());
+// console.log(myDate.getDay());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.getTime());
+
+// let myCreatedDate = new Date(2023, 0, 23);
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3);
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate = new Date("01-14-2023");
+// console.log(myCreatedDate.toLocaleString());
+
+let myCreatedDate = new Date("2023-01-14");
 // console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now();
-
 // console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+// console.log(Date.now());
+// console.log(Date.now() / 1000); // but problem is it gives result in decimal
+// console.log(Math.floor(Date.now() / 1000));
 
 let newDate = new Date();
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
 
-// `${newDate.getDay()} and the time `
+// `${newDate.getDay()} and the time is`;
 
 newDate.toLocaleString("default", {
   weekday: "long",
