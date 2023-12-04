adc690582de270a769c6bb4ff556838c65b3db98
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-12-04 23:08:12 +0530

    Update 03_objects.js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,1 +1,47 @@
 //singleton
+
+// object literals
+
+//const JsUser = {}
+
+//Object.create
+
+const mySym = Symbol("key1");
+
+const JsUser = {
+  name: "Hitesh",
+  "full name ": "Hitesh Choudhary",
+  [mySym]: "mykey1",                         // [mysm] => [ ] syntax for symbol
+  age: 18,
+  location: "Jaipur",
+  email: "hitesh@google.com",
+  isloggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"],
+};
+
+console.log(JsUser.email); //hitesh@google.com
+console.log(JsUser["email"]); //hitesh@google.com
+// Above these are two way to access elements lower one is preffered.
+
+console.log(JsUser["full name "]);
+console.log(JsUser[mySym]);
+console.log(typeof JsUser.mySym); 
+
+JsUser.email = "hitesh@chatgpt.com";
+Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+console.log(JsUser);
+
+/*outout:
+{
+  name: 'Hitesh',
+  'full name ': 'Hitesh Choudhary',
+  age: 18,
+  location: 'Jaipur',
+  email: 'hitesh@chatgpt.com',
+  isloggedIn: false,
+  lastLoginDays: [ 'Monday', 'Saturday' ],
+  [Symbol(key1)]: 'mykey1'
+}
+*/
+
